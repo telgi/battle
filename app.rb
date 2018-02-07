4a9e260e1e365157d10f91f808c530907542b409
Terry Allitt <tallitt88@gmail.com>
2018-02-07 15:54:21 +0000

    Change names.erb to play.erb

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1 = @params[:player_1]
-    @player_2 = @params[:player_2]
-    erb(:names)
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
   end
 
   run! if app_file == $0
