4571b5d31076de927361b29894fa7b575ef67308
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-08 11:21:19 +0000

    added web helpers to use methods in feature tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit '/'
-    fill_in :player_1, with: 'Terry'
-    fill_in :player_2, with: 'LoolouMcgoo'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Terry VS LoolouMcgoo'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'user displays hit points' do
   scenario 'player 2 displays hit points' do
-    visit '/'
-    fill_in :player_1, with: 'Terry'
-    fill_in :player_2, with: 'LoolouMcgoo'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'LoolouMcgoo : 100hp'
   end
 end
